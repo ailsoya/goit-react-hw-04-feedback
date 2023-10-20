6d7c3d313d30799c492945ea47c22f8346e0e738
ailsoya <akalitata53@gmail.com>
2023-10-20 14:32:31 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-direct-mutation-state */
 import { useState } from "react"
-import { Component } from 'react'
 import { FeedbackOptions } from './Elements/Buttons'
 import { Statistics } from './Elements/Statistics'
 import { Section } from './Elements/Section'
@@ -53,56 +52,3 @@ export const App = () => {
     </>
   )
 }
-
-/*export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  }
-
-  handleFeedback = (name) => {
-    this.setState(prev =>({
-      [name.name]: prev[name.name] + 1,
-    }))
-  }
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state
-    const total = good + neutral + bad
-    return total
-  }
-
-  countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state
-    const total = good + neutral + bad
-    return Math.round((good / total) * 100)
-  }
-
-  render() {
-    const { good, neutral, bad } = this.state
-    const total = this.countTotalFeedback()
-    const totalPercentage = this.countPositiveFeedbackPercentage()
-
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions onClick={this.handleFeedback} options={['good', 'neutral', 'bad']}/>
-        </Section>
-        <Section title="Statistics">
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              percentage={totalPercentage}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </>
-    );
-  }
-}*/
